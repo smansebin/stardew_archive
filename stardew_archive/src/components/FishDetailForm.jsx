286ed5ab410a75a9cc486215b03c9486018ab91a
ihye589 <ihye589@gmail.com>
2026-08-01 21:21:11 +0900

    낚시 상세폼/계절

--- stardew_archive/src/components/FishDetailForm.jsx
+++ stardew_archive/src/components/FishDetailForm.jsx
@@ -1,12 +1,28 @@
+import {useState} from 'react'
 import './FishDetailForm.css'
 
+const SEASONS = ['봄','여름','가을','겨울']
+
 export default function FishDetailForm({fish, onClose}){
+    const[seasons, setSeasons ]=useState([])
     return(
         <div className = "detail-overlay" onClick={onClose}>
             <div className = "detail-form" onClick ={(e) => e.stopPropagation()}>
                 <h3>{fish.name}</h3>
                 <img src = {fish.image} alt ={fish.name} width = {80}/>
-                <p>=상세 정보 입력폼=</p>
+                <p>계절</p>
+                {SEASONS.map((season) => (
+                <button
+                key={season} /* 이름표 */
+                onClick={() =>
+                    setSeasons((prev) =>
+                    prev.includes(season) ? prev.filter((s) => s !== season) : [...prev, season]
+                    )
+                }
+                >
+                {season} {seasons.includes(season) ? '✓' : ''}
+                </button>
+            ))}
                 <button onClick={onClose} >닫기</button>
             </div>
         </div>
