7a6e92d6a55cda143da5590a16278671d93e0469
smansebin <smansebin25@sookmyung.ac.kr>
2026-08-05 17:02:52 +0900

    chore: bundleData, cropData 완성

--- stardew_archive/src/data/cookingData.js
+++ stardew_archive/src/data/cookingData.js
@@ -28,7 +28,148 @@ export const cookingData = [
         id: 6,
         name: "설탕당근 스프",
         image: "/images/cooking/Parsnip_Soup.png"
-    }
+    },
+    {
+        id: 7,
+        name: "야채의 메들리",
+        image: "/images/cooking/Vegetable_Medley.png"
+    },
+    {
+        id: 8,
+        name: "완벽한 아침",
+        image: "/images/cooking/Complete_Breakfast.png"
+    },
+    {
+        id: 9,
+        name: "오징어튀김",
+        image: "/images/cooking/Fried_Calamari.png"
+    },
+    {
+        id: 10,
+        name: "수상한 롤",
+        image: "/images/cooking/Strange_Bun.png"
+    },
+    {
+        id: 11,
+        name: "행운의 점심",
+        image: "/images/cooking/Lucky_Lunch.png"
+    },
+    {
+        id: 12,
+        name: "버섯구이",
+        image: "/images/cooking/Fried_Mushroom.png"
+    },
+    {
+        id: 13,
+        name: "피자",
+        image: "/images/cooking/Pizza.png"
+    },
+    {
+        id: 14,
+        name: "콩 스튜",
+        image: "/images/cooking/Bean_Hotpot.png"
+    },
+    {
+        id: 15,
+        name: "맛탕",
+        image: "/images/cooking/Glazed_Yams.png"
+    },
+    {
+        id: 16,
+        name: "깜짝잉어",
+        image: "/images/cooking/Carp_Surprise.png"
+    },
+    {
+        id: 17,
+        name: "해시브라운",
+        image: "/images/cooking/Hashbrowns.png"
+    },
+    {
+        id: 18,
+        name: "팬케이크",
+        image: "/images/cooking/Pancakes.png"
+    },
+    {
+        id: 19,
+        name: "연어 정찬",
+        image: "/images/cooking/Salmon_Dinner.png"
+    },
+    {
+        id: 20,
+        name: "생선 타코",
+        image: "/images/cooking/Fish_Taco.png"
+    },
+    {
+        id: 21,
+        name: "우럭 튀김",
+        image: "/images/cooking/Crispy_Bass.png"
+    },
+    {
+        id: 22,
+        name: "페퍼 파퍼",
+        image: "/images/cooking/Pepper_Poppers.png"
+    },
+    {
+        id: 23,
+        name: "빵",
+        image: "/images/cooking/Bread.png"
+    },
+    {
+        id: 24,
+        name: "똠카 스프",
+        image: "/images/cooking/Tom_Kha_Soup.png"
+    },
+    {
+        id: 25, //송어 스프
+        name: "송어 스프",
+        image: "/images/cooking/Trout_Soup.png"
+    },
+    {
+        id: 26,
+        name: "초콜릿 케이크",
+        image: "/images/cooking/Chocolate_Cake.png"
+    },
+    {
+        id: 27,
+        name: "핑크 케이크",
+        image: "/images/cooking/Pink_Cake.png"
+    },
+    {
+        id: 28, //대황 파이
+        name: "대황 파이",
+        image: "/images/cooking/Rhubarb_Pie.png"
+    },
+    {
+        id: 29, //쿠키
+        name: "쿠키",
+        image: "/images/cooking/Cookie.png"
+    },
+    {
+        id: 30,
+        name: "스파게티",
+        image: "/images/cooking/Spaghetti.png"
+    },
+    {
+        id: 31,
+        name: "장어튀김",
+        image: "/images/cooking/Fried_Eel.png"
+    },
+    {
+        id: 32, //매콤한 장어
+        name: "매콤한 장어",
+        image: "/images/cooking/Spicy_Eel.png"
+    },
+    {
+        id: 33,
+        name: "회",
+        image: "/images/cooking/Sashimi.png"
+    },
+    {
+        id: 34,
+        name: "마키 롤",
+        image: "/images/cooking/Maki_Roll.png"
+    },
+
 ]
 
 export default cookingData
--- stardew_archive/src/data/cropData.js
+++ stardew_archive/src/data/cropData.js
@@ -28,6 +28,207 @@ export const cropData = [
         id: 6,
         name: "마늘",
         image: "/images/crops/Garlic.png"
+    },
+    {
+        id: 7,
+        name: "완두콩",
+        image: "/images/crops/Green_Bean.png"
+    },
+    {
+        id: 8,
+        name: "커피콩",
+        image: "/images/crops/Coffee_Bean.png"
+    },
+    {
+        id: 9,
+        name: "케일",
+        image: "/images/crops/Kale.png"
+    },
+    {
+        id: 10,
+        name: "콜리플라워",
+        image: "/images/crops/Cauliflower.png"
+    },
+    {
+        id: 11,
+        name: "튤립",
+        image: "/images/crops/Tulip.png"
+    },
+    {
+        id: 12,
+        name: "파스닙",
+        image: "/images/crops/Parsnip.png"
+    },
+    {
+        id: 13,
+        name: "푸른 재즈",
+        image: "/images/crops/Blue_Jazz.png"
+    },
+    {
+        id: 14,
+        name: "매운 고추",
+        image: "/images/crops/Hot_Pepper.png"
+    },
+    {
+        id: 15,
+        name: "멜론",
+        image: "/images/crops/Melon.png"
+    },
+    {
+        id: 16,
+        name: "무",
+        image: "/images/crops/Radish.png"
+    },
+    {
+        id: 17,
+        name: "밀",
+        image: "/images/crops/Wheat.png"
+    },
+    {
+        id: 18,
+        name: "붉은 양배추",
+        image: "/images/crops/Red_Cabbage.png"
+    },
+    {
+        id: 19,
+        name: "블루베리",
+        image: "/images/crops/Blueberry.png"
+    },
+    {
+        id: 20,
+        name: "스타푸르트",
+        image: "/images/crops/Starfruit.png"
+    },
+    {
+        id: 21,
+        name: "양귀비",
+        image: "/images/crops/Poppy.png"
+    },
+    {
+        id: 22,
+        name: "여름별꽃",
+        image: "/images/crops/Summer_Spangle.png"
+    },
+    {
+        id: 23,
+        name: "여름호박",
+        image: "/images/crops/Summer_Squash.png"
+    },
+    {
+        id: 24,
+        name: "옥수수",
+        image: "/images/crops/Corn.png"
+    },
+    {
+        id: 25,
+        name: "토마토",
+        image: "/images/crops/Tomato.png"
+    },
+    {
+        id: 26,
+        name: "해바라기",
+        image: "/images/crops/Sunflower.png"
+    },
+    {
+        id: 27,
+        name: "홉",
+        image: "/images/crops/Hops.png"
+    },
+    {
+        id: 28,
+        name: "가지",
+        image: "/images/crops/Eggplant.png"
+    },
+    {
+        id: 29,
+        name: "브로콜리",
+        image: "/images/crops/Broccoli.png"
+    },
+    {
+        id: 30,
+        name: "사탕무",
+        image: "/images/crops/Beet.png"
+    },
+    {
+        id: 31,
+        name: "아마란스",
+        image: "/images/crops/Amaranth.png"
+    },
+    {
+        id: 32,
+        name: "아티초크",
+        image: "/images/crops/Artichoke.png"
+    },
+    {
+        id: 33,
+        name: "요정장미",
+        image: "/images/crops/Fairy_Rose.png"
+    },
+    {
+        id: 34,
+        name: "참마",
+        image: "/images/crops/Yam.png"
+    },
+    {
+        id: 35,
+        name: "청경채",
+        image: "/images/crops/Bok_Choy.png"
+    },
+    {
+        id: 36,
+        name: "크렌베리",
+        image: "/images/crops/Cranberry.png"
+    },
+    {
+        id: 37,
+        name: "포도",
+        image: "/images/crops/Grape.png"
+    },
+    {
+        id: 38,
+        name: "호박",
+        image: "/images/crops/Pumpkin.png"
+    },
+    {
+        id: 39,
+        name: "파우더멜론",
+        image: "/images/crops/Powdermelon.png"  
+    },
+    // TODO: 같은 형식으로 고대 과일, 선인장 열매, 섬유, 씨앗 모음, 파인애플, 달콤보석베리, 찻잎 데이터 추가
+    {
+        id: 40,
+        name: "고대 과일",
+        image: "/images/crops/Ancient_Fruit.png"
+    },
+    {
+        id: 41,
+        name: "선인장 열매",
+        image: "/images/crops/Cactus_Fruit.png"
+    },
+    {
+        id: 42,
+        name: "섬유",
+        image: "/images/crops/Fiber.png"
+    },
+    {
+        id: 43,
+        name: "씨앗 모음",
+        image: "/images/crops/Mixed_Seeds.png"
+    },
+    {
+        id: 44,
+        name: "파인애플",
+        image: "/images/crops/Pineapple.png"
+    },
+    {
+        id: 45,
+        name: "달콤보석베리",
+        image: "/images/crops/Sweet_Gem_Berry.png"
+    },
+    {
+        id: 46,
+        name: "찻잎",
+        image: "/images/crops/Tea_Leaves.png"
     }
 ]
 
